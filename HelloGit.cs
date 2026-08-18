b7fe4734a5325636b63ccefac5c3cf0f3e803480
Jnaneshwar <dnyan.006@gmail.com>
2026-08-18 16:04:18 +0400

    Add HelloGit example

---
+++ HelloGit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitLearning
+{
+    public class HelloGit
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello from Git!");
+        }
+    }
+}
